2a3e1932bab1408ae8c399cd198dd5e55a7d4541
Jaime Sotomayor <89789808+JSotomayr@users.noreply.github.com>
2021-11-26 12:48:36 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,21 +6,20 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
@@ -65,7 +64,7 @@ def delete_member(id):
     
     return jsonify({'error':'Details not found'}), 400
 
-# this only runs if `$ python src/app.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
